12629538411d84feba733cb34b040e6d7d25d44b
Erin Anderson <e.anderson1226@gmail.com>
2020-11-05 07:45:29 -0500

    commit tree a2bb74464fc631c992f835a53c94472c931f0627

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,31 @@
 
+class CashRegister
+
+    attr_accessor :total, :discount, :previous_transaction, :items
+
+    def initialize(store_discount=0)
+        @total = 0
+        @discount = store_discount
+        @items = []
+    end
+    
+    def total
+        @total
+    end
+
+    def add_item(title, price, quantity=1)
+        self.total += price * quantity
+        self.previous_transaction = price * quantity
+    end
+
+    def apply_discount
+        @total = self.total * discount
+    end
+
+    def items
+    end
+
+    def void_last_transaction
+    end
+
+end
